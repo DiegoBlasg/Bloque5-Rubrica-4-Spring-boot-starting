a2db92239121c85f0ea0c668f807f8d3b0ff5f6f
DiegoBlasg <diego.blasg@gmial.com>
2023-02-05 13:35:04 +0100

    first commit

---
+++ src/test/java/com/concretepage/Bloque5Rubrica4/Bloque5Rubrica4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concretepage.Bloque5Rubrica4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque5Rubrica4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
